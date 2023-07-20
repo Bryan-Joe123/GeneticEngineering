62770d48a4eeb032a8812fecce6ccf1e44ec3db6
BRdev <75519189+bryanpy@users.noreply.github.com>
2023-07-20 17:53:39 +0530

    Add files via upload

---
+++ Scripts/more-content.js
@@ -0,0 +1,39 @@
+var selected = "GeneEng";
+function switchCont(context,id){
+    window.location = '#'+context+'-more';
+    document.getElementById(context+'-ToC').classList.add("selected");
+    document.getElementById(selected+'-ToC').classList.remove("selected");
+    selected = context
+}
+
+function toggleToC(){
+    var ToC = document.getElementById("table-contents");
+    if(ToC.classList.contains("table-contents")){
+        ToC.classList.add("table-contents-hide");
+        ToC.classList.remove("table-contents") 
+        console.log("lol");
+
+        var textCont = document.getElementsByClassName("text-container");
+        // textCont.width = "100vw"
+        console.log(textCont);
+
+        for (let x = 0; x < textCont.length; x++) {
+            const element = textCont[x];
+            element.style.width = "90vw"
+        }
+    }
+    else if(ToC.classList.contains("table-contents-hide")){
+        ToC.classList.add("table-contents");
+        ToC.classList.remove("table-contents-hide") 
+        console.log("lol");
+
+        var textCont = document.getElementsByClassName("text-container");
+        // textCont.width = "100vw"
+        console.log(textCont);
+
+        for (let x = 0; x < textCont.length; x++) {
+            const element = textCont[x];
+            element.style.width = "73vw"
+        }
+    }
+}
