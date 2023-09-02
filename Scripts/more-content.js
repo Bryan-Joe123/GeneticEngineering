37d3aaadfa0e88017461f5ae4a6885e068404305
BRdev <75519189+bryanpy@users.noreply.github.com>
2023-09-03 00:02:39 +0400

    Add files via upload

--- Scripts/more-content.js
+++ Scripts/more-content.js
@@ -1,40 +1,16 @@
 var selected = "GeneEng";
-function switchCont(context,id){
-    window.location = '#'+context+'-more';
-    document.getElementById(context+'-ToC').classList.add("selected");
-    document.getElementById(selected+'-ToC').classList.remove("selected");
-    selected = context
-}
 
-function toggleToC(){
-    var ToC = document.getElementById("table-contents");
-    if(ToC.classList.contains("table-contents")){
-        ToC.classList.add("table-contents-hide");
-        ToC.classList.remove("table-contents") 
-        console.log("lol");
+function switchContent(id,selfid){
+    Array.from(document.getElementById('more-content').children).forEach(element => {
+        element.classList.remove("more-content-show")
+    });
 
-        var textCont = document.getElementsByClassName("text-container");
-        // textCont.width = "100vw"
-        console.log(textCont);
+    Array.from(document.getElementById('selection-container').children).forEach(element => {
+        element.classList.remove("selected")
+    });
 
-        for (let x = 0; x < textCont.length; x++) {
-            const element = textCont[x];
-            // element.style.width = "90vw"
-            setTimeout(element.style.width = "90vw", 1000*x);
-        }
-    }
-    else if(ToC.classList.contains("table-contents-hide")){
-        ToC.classList.add("table-contents");
-        ToC.classList.remove("table-contents-hide") 
-        console.log("lol");
+    selected = id
 
-        var textCont = document.getElementsByClassName("text-container");
-        // textCont.width = "100vw"
-        console.log(textCont);
-
-        for (let x = 0; x < textCont.length; x++) {
-            const element = textCont[x];
-            element.style.width = "70vw"
-        }
-    }
+    document.getElementById(id).classList.add("more-content-show")
+    document.getElementById(selfid).classList.add("selected")
 }
