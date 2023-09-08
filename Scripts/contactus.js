23c7f71223c75e01de3ca0a50e5f135641a446f8
BRdev <75519189+bryanpy@users.noreply.github.com>
2023-09-08 11:58:13 +0400

    Add files via upload

--- Scripts/contactus.js
+++ Scripts/contactus.js
@@ -4,28 +4,49 @@ var subject;
 var email;
 var message;
 
-function sendEmail() {
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "bryanjoeadams@gmail.com",
-        Password : "7A62EEC25F0FC18598BC7932C2BFB302DCAC",
-        To: 'bryanjoeadams@gmail.com',
-        From: 'bryanjoeadams@gmail.com',
-        Subject: subject,
-        Body: "Name:${uname}\nPhone:${phone}\nEmail:${email}\n${message}",
-    }).then(function (message) {
-        alert(message);
-        alert("Updated")
-    });
-}
 
-function submit(){
-    uname = document.getElementById('name').value;
-    phone = document.getElementById('phone').value;
-    subject = document.getElementById('subject').value;
-    email = document.getElementById('email').value;
-    message = document.getElementById('message').value;
 
-    sendEmail();
+window.addEventListener("DOMContentLoaded", (event) => {
+    let subject_field = document.getElementById('subject')
+    let message_field = document.getElementById('message')
 
-}
+    subject_field.addEventListener("change",(e)=>{
+        subject = document.getElementById('subject').value;
+        // console.log(message);
+
+        document.getElementById('submit-button').setAttribute('href', "mailto: bryanjoeadams@gmail.com?subject="+subject+'&body='+message+'')
+    })
+
+    message_field.addEventListener("change",(e)=>{
+        message = document.getElementById('message').value;
+        // console.log(message);
+
+        document.getElementById('submit-button').setAttribute('href', "mailto: bryanjoeadams@gmail.com?subject="+subject+'&body='+message+'')
+    })
+});
+
+// function sendEmail() {
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "bryanjoeadams@gmail.com",
+//         Password : "7A62EEC25F0FC18598BC7932C2BFB302DCAC",
+//         To: 'bryanjoeadams@gmail.com',
+//         From: 'bryanjoeadams@gmail.com',
+//         Subject: subject,
+//         Body: "Name:${uname}\nPhone:${phone}\nEmail:${email}\n${message}",
+//     }).then(function (message) {
+//         alert(message);
+//         // alert("Updated")
+//     });
+// }
+
+// function submit(){
+//     uname = document.getElementById('name').value;
+//     phone = document.getElementById('phone').value;
+//     subject = document.getElementById('subject').value;
+//     email = document.getElementById('email').value;
+//     message = document.getElementById('message').value;
+
+//     sendEmail();
+
+// }
